1e795b7b4541dd3c86007d76e9f547b6b15d3768
sam <ccchaovvv@163.com>
2024-07-16 23:32:59 +0800

    juc案例

--- heima/reflection/src/main/java/proxy/Test.java
+++ heima/reflection/src/main/java/proxy/Test.java
@@ -3,7 +3,8 @@ package proxy;
 public class Test {
     public static void main(String[] args) {
         Star star = ProxyUtils.createProxy(new BigStar("ikun"));
-        star.sing("hello");
-        star.dance("fuck");
+        String hello = star.sing("hello");
+        String fuck = star.dance("fuck");
+        System.out.println(hello + fuck);
     }
 }
