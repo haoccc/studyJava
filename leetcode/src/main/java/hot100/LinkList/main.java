ac919966aabe36c534dda803513bced4a180e2bf
sam <ccchaovvv@163.com>
2024-10-29 00:39:36 +0800

    hot 100 二叉树

--- leetcode/src/main/java/hot100/LinkList/main.java
+++ leetcode/src/main/java/hot100/LinkList/main.java
@@ -1,8 +1,6 @@
 package hot100.LinkList;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class main {
 
@@ -185,10 +183,156 @@ public class main {
         return pre.next;
     }
 
-    public static void main(String[] args) {
-        ListNode listNode2 = new ListNode(2, null);
-        ListNode listNode1 = new ListNode(1, listNode2);
+    /**
+     * 2. 两数相加
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int n = 0;
+        ListNode head = l1;
+        while (l1.next != null && l2.next != null){
+            int val = l1.val + l2.val + n;
+            l1.val = val % 10;
+            n = val / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        int v = l1.val + l2.val + n;
+        l1.val = v % 10 ;
+        n = v / 10;
+        l1.next = l1.next != null? l1.next: l2.next;
 
-        hasCycle(listNode1);
+        while (n > 0 && l1.next != null){
+            int val = l1.next.val + n;
+            l1.next.val = val % 10;
+            n = val / 10;
+            l1 = l1.next;
+        }
+        if (n >= 1) l1.next = new ListNode(n);
+
+        return head;
+    }
+
+    /**
+     * 两数相加
+     * 简单版本
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode pre = head;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0){
+            int val1 = l1 != null? l1.val: 0;
+            int val2 = l2 != null? l2.val: 0;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            head.next = new ListNode();
+            head.next.val = sum % 10;
+            head = head.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return pre.next;
+
+    }
+
+    /**
+     * 19. 删除链表的倒数第 N 个结点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode(0, head);
+        ListNode fast = pre;
+        ListNode slow = pre;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return pre.next;
+    }
+
+
+    /**
+     * 24. 两两交换链表中的节点
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+//        ListNode pre = new ListNode(0, head);
+//        ListNode prest = pre;
+//
+//        while (pre.next != null  && pre.next.next != null){
+//            ListNode temp = pre.next;
+//            pre.next = temp.next;
+//            temp.next = pre.next.next;
+//            pre.next.next = temp;
+//            pre = pre.next.next;
+//        }
+//        return  prest.next;
+        if (head == null || head.next == null) return head;
+        ListNode temp = head.next;
+        head.next = swapPairs(temp.next);
+        temp.next = head;
+        return temp;
+    }
+
+
+    /**
+     * 148. 排序链表
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        // 冒泡
+        ListNode pre = new ListNode(0, head);
+        ListNode node = head;
+        while (node == null){
+            ListNode n = node;
+            while (n.next != null){
+                if (n.val < n.next.val){
+                    ListNode temp = n.next.next;
+                    n.next.next = n;
+                    pre.next = n.next;
+                    n.next = temp;
+
+                    pre = pre.next;
+                    n = pre.next;
+                }
+            }
+            node = node.next;
+        }
+        return head;
+    }
+
+
+    public static ListNode ArrayToListNode(int[] array){
+        ListNode head = new ListNode(array[0]);
+        ListNode pre = head;
+        for (int i = 1; i < array.length; i++) {
+            pre.next = new ListNode(array[i]);
+            pre = pre.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = ArrayToListNode(new int[]{3, 7});
+        ListNode l2 = ArrayToListNode(new int[]{9, 2});
+        main main = new main();
+        main.addTwoNumbers2(l1, l2);
     }
 }
---
+++ leetcode/src/main/java/hot100/Tree/main.java
@@ -0,0 +1,247 @@
+package hot100.Tree;
+
+import hot100.LinkList.ListNode;
+import study.tree.TreeNode;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.RecursiveAction;
+
+public class main {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return depth(root, 1);
+    }
+
+    public int depth(TreeNode node, int n){
+        int leftDepth = node.left != null? depth(node.left, n+1): n;
+        int rightDepth = node.right != null? depth(node.right, n+1): n;
+        return Math.max(leftDepth, rightDepth);
+    }
+
+    /**
+     * 226. 翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+    /**
+     * 101. 对称二叉树
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+
+//        recursiveIsSymmetric(root.left, root.right);
+        while (true){
+
+        }
+    }
+
+    public boolean  recursiveIsSymmetric(TreeNode left, TreeNode right){
+        if (left == null && right == null) return true;
+        else if (right == null || left == null || left.val != right.val) return false;
+        return recursiveIsSymmetric(left.left, right.right) & recursiveIsSymmetric(left.right, right.left);
+    }
+
+    /**
+     * 二叉树的直径
+     * @param root
+     * @return
+     */
+    int maxD = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter(root);
+        return maxD;
+    }
+
+    private int maxDiameter(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int left = root.left==null? 0: maxDiameter(root.left) + 1;
+        int right = root.right == null? 0: maxDiameter(root.right) + 1;
+        maxD = Math.max(left + right, maxD);
+        return Math.max(left, right);
+    }
+
+    /**
+     * 102. 二叉树的层序遍历
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            // 直接读取当前的 queue尺寸
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+/*        if (root == null) return new ArrayList<>();
+        Deque<TreeNode> queue = new LinkedList();
+        queue.addLast(root);
+        queue.addLast(null);
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> secondList = new ArrayList<>();
+        while (!queue.isEmpty()){
+            TreeNode poll = queue.poll();
+            if (poll == null){
+                list.add(secondList.stream().toList());
+                secondList.clear();
+                if (!queue.isEmpty()) queue.offer(null);
+            }else {
+                secondList.add(poll.val);
+                if (poll.left != null) queue.offer(poll.left);
+                if (poll.right != null) queue.offer(poll.right);
+            }
+        }
+        return list;*/
+    }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return recursiveBST(nums, 0, nums.length-1);
+    }
+
+    private TreeNode recursiveBST(int[] nums, int start, int end){
+        if (start == end) return new TreeNode(nums[start]);
+        else if (end < start) return null;
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = recursiveBST(nums, start, mid -1);
+        root.right = recursiveBST(nums, mid + 1, end);
+        return root;
+    }
+
+    /**
+     * 98. 验证二叉搜索树
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        // 中序遍历
+        List<Integer> list = new ArrayList<>();
+        recursiveIsValidBST(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) return false;
+        }
+        return true;
+    }
+
+    private void recursiveIsValidBST(TreeNode root, List<Integer> list){
+        if (root == null) return;
+        recursiveIsValidBST(root.left, list);
+        list.add(root.val);
+        recursiveIsValidBST(root.right, list);
+    }
+
+    /**
+     * 230. 二叉搜索树中第 K 小的元素
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        // 中序遍历, 迭代
+
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        while (root != null || !stack.isEmpty()){
+            while (root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+        return 0;
+    }
+
+
+    /**
+     * 199. 二叉树的右视图
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        // 层序遍历最后一个
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.offer(root);
+        while (!stack.isEmpty()){
+            Deque<TreeNode> stack2 = new LinkedList<>();
+            res.add(stack.getFirst().val);
+            while (!stack.isEmpty()){
+                TreeNode node = stack.poll();
+                if (node.right != null) stack2.offer(node.right);
+                if (node.left != null) stack2.offer(node.left);
+            }
+            stack = stack2;
+        }
+        return res;
+    }
+
+    /**
+     * 114. 二叉树展开为链表
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        recursiveFlatten(root);
+    }
+
+    private TreeNode recursiveFlatten(TreeNode root){
+        if (root == null) return root;
+        TreeNode right = recursiveFlatten(root.right);
+        TreeNode left = recursiveFlatten(root.left);
+        root.right = left;
+        if (left == null) return root;
+        while (left.right != null) left = left.right;
+        left.right = right;
+        return root;
+    }
+
+    public static void main(String[] args) {
+        main main = new main();
+        TreeNode treeNode15 = new TreeNode(15);
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode20 = new TreeNode(20);
+        treeNode20.left = treeNode15;
+        treeNode20.right = treeNode7;
+
+        TreeNode treeNode9 = new TreeNode(9);
+        TreeNode treeNode3 = new TreeNode(3);
+        treeNode3.left = treeNode9;
+        treeNode3.right = treeNode20;
+
+        TreeNode nullNode = null;
+        main.levelOrder(nullNode);
+    }
+}
